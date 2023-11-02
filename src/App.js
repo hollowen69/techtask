3c10e4296708b8bdcce8b1a8a5b3c1645077f72d
Elmehdi salim <el-mehdi.salim@akkodis.com>
2023-11-02 16:52:15 +0100

    added paypal button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/pages/Login';
 import Home from './components/pages/Home';
 import SignUp from './components/pages/SignUp';
 import Cart from './components/pages/Cart';
+import PaypalTest from './components/pages/PaypalTest';
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       <Route path="" element={<Home />} />
       <Route path="/Cart" element={<Cart />} />
       <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/Paypal" element={<PaypalTest />} />
       </Routes>
     </Router>
   );
---
+++ src/components/pages/PaypalTest/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import PaypalButton from '../../atoms/PaypalButton';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+
+const PaymentForm = () => {
+  
+
+  return (
+    
+    <PaypalButton />
+  );
+};
+
+export default PaymentForm;
